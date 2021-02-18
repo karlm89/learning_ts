7ea1604e716d879388cc8d2883f5eb140caed1b1
Karl Murray <karlm89@me.com>
2021-02-18 07:50:44 -0600

    Finish ep 14

--- 2_types/app.ts
+++ 2_types/app.ts
@@ -1,9 +1,15 @@
-function add(num1:number, num2:number){
-    return num1 + num2
+function add(num1:number, num2:number, showResult: boolean, phrase: string){
+    const result = num1 + num2
+    if(showResult){
+       console.log(phrase + result)
+    } else {
+        return result
+    }
 }
 
 const number1 = 5
 const number2 = 2.8
+const printResult = true
+const resultPhrase = 'The Result is : '
 
-const result = add(number1, number2)
-console.log(result)
+add(number1, number2, printResult, resultPhrase)
